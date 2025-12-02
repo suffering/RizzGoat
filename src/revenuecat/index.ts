2d4e1adffb10bedf604930d6181f9a99a57e70ff
suffering <95081939+suffering@users.noreply.github.com>
2025-12-02 17:09:53 -0500

    Refactor RevenueCat initialization to a function

--- src/revenuecat/index.ts
+++ src/revenuecat/index.ts
@@ -1,19 +1,15 @@
 import { Platform } from 'react-native';
-import { useEffect } from 'react';
 import Purchases, { LOG_LEVEL } from 'react-native-purchases';
 
-export default function App() {
-  useEffect(() => {
-    Purchases.setLogLevel(LOG_LEVEL.VERBOSE);
+export function initRevenueCat() {
+  Purchases.setLogLevel(LOG_LEVEL.VERBOSE);
 
-    // Platform-specific API keys
-    const iosApiKey = 'test_rtpJYQRunpmnybPXjhHVzzuWZwy';
-    const androidApiKey = 'test_rtpJYQRunpmnybPXjhHVzzuWZwy';
+  const iosApiKey = 'test_rtpJYQRunpmnybPXjhHVzzuWZwy';
+  const androidApiKey = 'test_rtpJYQRunpmnybPXjhHVzzuWZwy';
 
-    if (Platform.OS === 'ios') {
-       Purchases.configure({apiKey: iosApiKey});
-    } else if (Platform.OS === 'android') {
-       Purchases.configure({apiKey: androidApiKey});
-    }
-  }, []);
+  if (Platform.OS === 'ios') {
+    Purchases.configure({ apiKey: iosApiKey });
+  } else {
+    Purchases.configure({ apiKey: androidApiKey });
+  }
 }
