5a0928abd60bf1cd9d9978ce2664ba04db4e6526
suffering <95081939+suffering@users.noreply.github.com>
2025-12-04 11:43:43 -0500

    Add platform-specific extensions to module resolver
    Updated module resolver extensions to include platform-specific TypeScript files.

--- babel.config.js
+++ babel.config.js
@@ -3,16 +3,30 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
+      // Required by expo-router
+      require.resolve("expo-router/babel"),
+
+      // Handle "@/..." imports
       [
         "module-resolver",
         {
           root: ["./"],
-          extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+          extensions: [
+            ".ios.ts",
+            ".android.ts",
+            ".ts",
+            ".tsx",
+            ".js",
+            ".jsx",
+            ".json"
+          ],
           alias: {
             "@": "./"
           }
         }
       ],
+
+      // Keep this last
       "react-native-reanimated/plugin"
     ]
   };
